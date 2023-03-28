ac87a4f5aa11d840ad9e553af7e74976176e621a
polashbiswas <30291056+polashbiswas@users.noreply.github.com>
2023-03-29 01:19:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 function calculateMinCost() {
   //your code here
-  
-  
+  let inputData = document.querySelector("#rope-lengths").value;
+	let inputArr = inputData.split(",");
+	for(let i = 0; i < inputArr.length; i++){
+		inputArr[i] = Number(inputArr[i]);
+	}
+  var cost = 0;
+	inputArr.sort(function(a,b) { return a-b});
+	while (inputArr.length > 1) {
+		let newRope = inputArr[0] + inputArr[1];
+		cost += newRope;
+
+		//delete first two element
+		inputArr.splice(0,2);
+		inputArr.push(newRope);
+		inputArr.sort(function(a,b) { return a-b});
+	}
+	document.querySelector("#result").textContent = cost;
   
 }  
